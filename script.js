beccca950c1c1468cff1a8425d79fa2f729b710a
AbzMijon <egoraksevic@gmail.com>
2022-04-10 15:15:12 +0300

    in progress

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    keyboard: {
+        OnlyInViewport:true,
+    },
+    slidesPerView: 3,
+});
