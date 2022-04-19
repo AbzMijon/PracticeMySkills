30231a95e7a7e22ca6c9e595f07a88961740c072
AbzMijon <egorchiktop228@gmail.com>
2022-04-19 13:12:22 +0300

    in progress

--- script.js
+++ script.js
@@ -12,3 +12,53 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
 });
 
+
+//Homework
+
+//Task 1
+const person = {
+    name: 'John' ,
+    age: 22
+}
+delete person.age;
+delete person.name;
+console.log(person);
+
+//Task 2
+const carTools = {
+    wheels: 4 ,
+    seats: 5
+}
+console.log('wheels' in carTools);
+
+//Task 3
+const student = {
+    name: 'John',
+    age: 19,
+    isHappy: true
+}
+console.log(Object.keys(student)); //Намного проще)
+console.log(Object.values(student));
+
+//Или
+
+for (const key in student) {
+    console.log(key);
+}
+for (const key in student) {
+    console.log(student[key]);
+}
+
+//Task 4
+const colors = {
+    'ru pum pu ru rum': {
+        red: 'красный',
+        green: 'зеленый',
+        blue: 'синий'
+    }
+};
+console.log(colors['ru pum pu ru rum'].red , colors['ru pum pu ru rum'].blue);
+
+//Task 5
+
+
