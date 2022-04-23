fa7bd6c30b4f9bdfc607cf1a587c8bc688308347
AbzMijon <egorchiktop228@gmail.com>
2022-04-23 13:42:12 +0300

    massives hometask

--- script.js
+++ script.js
@@ -13,205 +13,105 @@ const swiper = new Swiper('.swiper', {
 });
 
 
-
-//Homework
+//Hometask
 
 //Task 1
-const person = {
-    name: 'John' ,
-    age: 22
-}
-delete person.age;
-delete person.name;
-console.log(person);
+const colors = ['red', 'green', 'blue'];
+console.log(colors.length);
 
 //Task 2
-const carTools = {
-    wheels: 4 ,
-    seats: 5
-}
-console.log('wheels' in carTools);
+const animals = ['monkey', 'dog', 'cat'];
+console.log(animals[animals.length - 1]);
 
 //Task 3
-const student = {
-    name: 'John',
-    age: 19,
-    isHappy: true
-}
-console.log(Object.keys(student)); //Намного проще)
-console.log(Object.values(student));
+const numbers = [5, 43, 63, 23, 90];
+numbers.length = 0
+console.log(numbers);
 
-//Или
-
-for (const key in student) {
-    console.log(key);
-}
-for (const key in student) {
-    console.log(student[key]);
-}
+//Second Way (Task 3)
+console.log(numbers.splice(0 , numbers.length));
 
 //Task 4
-const colors = {
-    'ru pum pu ru rum': {
-        red: 'красный',
-        green: 'зеленый',
-        blue: 'синий'
-    }
-}
-console.log(colors['ru pum pu ru rum'].red , colors['ru pum pu ru rum'].blue);
+const students = ['Polina', 'Dasha', 'Masha'];
+students.pop(); students.push('Borya');
+students.shift(); students.unshift('Andrey');
+console.log(students);
 
 //Task 5
-const salaries = {
-    andrey: 500,
-    sveta: 413,
-    anton: 987,
-    andrey2: 664,
-    alexandra: 199
+const cats = ['Gachito', 'Tom', 'Batman'];
+for (let i = 0; i < cats.length; i++) { // For
+    console.log(cats[i]);
 }
-
-const generatorMiddleSalaries  = function () {
-    const valuesInSalaries = Object.values(salaries);
-    let middleSalaries = null;
-    for (let i = 0; i < valuesInSalaries.length; i++) {
-        middleSalaries = (middleSalaries + valuesInSalaries[i]);
-    }
-    console.log(middleSalaries / valuesInSalaries.length);
+for (const iterator of cats) { //For Of
+    console.log(iterator);
 }
-generatorMiddleSalaries();
 
 //Task 6
-let userName = prompt('Create Your Name!');
-let userPass = prompt('Now , create Your Password!');
-const userInfo = {
-    Name: userName ,
-    Password: userPass
-}
-let userNameAgain = prompt('Repeat Your Name..');
-let userPassAgain = prompt('Repeat Your Password..');
-if (userNameAgain === userInfo.Name && userPassAgain === userInfo.Password) alert(`Добро Пожаловать ${userInfo.Name}!`);
-else alert('Неверный Логин или Пороль!');
+const evenNumbers = [2, 4, 6, 8, 10];
+const oddNumbers = [1, 3, 5, 7, 9];
+const allNumbers = evenNumbers.concat(oddNumbers);
+console.log(allNumbers);
+console.log('Index Of number 8 = ' + allNumbers.indexOf(8) + 'rd');
 
-//Advanced Level ========================
+//Task 7
+const binary = [0, 0, 0, 0];
+console.log(String(binary).split(',').join('1'));
+
+//Advanced Level ==================
 
 //Task 1
-let firstTeamGoals = '2';
-let secondTeamGoals = '5';
-const obj = {
-    1:'Один',
-    2:'Два',
-    3:'Три',
-    4:'Четыре',
-    5:'Пять',
-    6:'Шесть',
-    7:'Семь',
-    8:'Восемь',
-    9:'Девять'
+const PALINDROM = function (str) {
+    let comparison = str.split('').reverse().join('');
+    if (str === comparison) alert('It is a Palindrom!')
+    else alert('It is NOT a Palindrom!');
 }
-const objValues = Object.values(obj);
-for (let i = 0; i < objValues.length; i++) {
-    if (firstTeamGoals == i) console.log(objValues[i - 1]);
-    else if (secondTeamGoals == i) console.log(objValues[i - 1]);
-}
+PALINDROM('лимузинизумил');
 
 //Task 2
-const student1 = {
-    name: 'Polina',
-    age: 27,
+const matrix = [
+    [12, 98, 78, 65, 23],
+    [54, 76, 98, 43, 65],
+    [13, 324, 65, 312],
+    [9092, 22, 45, 90000],
+]
+let allNums = matrix[0].concat(matrix[1] , matrix[2] , matrix[3]);
+let result = null;
+for (let i = 0; i < allNums.length; i++) {
+    result += allNums[i]
 }
-const student2 = {
-    name: 'Polina',
-    age: 27,
-}
-const firstFunction = Object.keys(student1) + Object.values(student1);
-const secondFunction = Object.keys(student2) + Object.values(student2);
-if (firstFunction === secondFunction) console.log(true);
-else console.log(false);
+console.log(result / allNums.length);
 
-
-////////// Practice JS
-
-//1
-const myOBJ2 = {
-    name: prompt('Enter your nick..') ,
-    age: +prompt('Enter your age..') ,
-    adminChecking: function () {
-        if (this.name === 'administrator' && this.age === 19) alert('Welcome ' + this.name + '!');
-        else alert(this.name + ' you Are NOT a Administrator');
-    }
+//Task 3
+const mixedNumbers = [-14, 24, -89, 43, 0 , -1, 412, 4];
+let posNum = [];
+let negNum = [];
+for (let i = 0; i < mixedNumbers.length; i++) {
+    if (mixedNumbers[i] >= 0) posNum.push(mixedNumbers[i]);
+    else negNum.push(mixedNumbers[i]);
 }
-console.log(myOBJ2.adminChecking());
+console.log(posNum);
+console.log(negNum);
 
-//2
-const constructure = function(name , age , work) {
-    const obj = {
-        name: name ,
-        age: age ,
-        work , work
-    }
-    if (work === 'web developer') {
-        for (let i = 0; i <= work.length; i++) {
-            console.log(`In this work ${work.length} words. They are ${work[i]}`);
-        }
-    }
+//Task 4
+const randomNumbers = [2 , 1 , 10 , 1 , 2];
+let mainResult = [];
+for (let i = 0; i < randomNumbers.length; i++) {
+    mainResult.push(randomNumbers[i]**2);
 }
-console.log(constructure('Egor' , 18 , 'web developer'));
+console.log(randomNumbers);
+console.log(mainResult);
 
-//3
-const workers = {
-    vlad: 200,
-    egor: 350,
-    artem: 120,
-    danik: 650,
-    adel: 1000
-}
-const middleRubles = function () {
-    const value = Object.values(workers);
-    let result = null;
-    for (let i = 0; i < value.length; i++) {
-        result = result + value[i];
-    }
-    console.log(result / value.length);
-}
-middleRubles();
+// Или же вот так что бы пользователь мог сам выбрать какие числа ему нужны)
 
-//4
-const bigObj = {
-    mainUser: {
-        mainInformation: {
-            name: 'Олег' ,
-            surname: 'Лавров' ,
-            age: 21 ,
-            isWife: true
-        } ,
-        progremmerSkills: {
-            javaScript: {
-                swiperJS: true ,
-                basics: true ,
-                frameworks: false
-            } ,
-            css: {
-                flexbox: true ,
-                gridLayuot: true ,
-                floats: false
-            } ,
-            html: {
-                allSkills: true
-            }
-        } ,
-        contacts: {
-            tel: +3753295720 ,
-            inst: '@oleg.mangol' ,
-            mail: 'oleg228.gmail.com'
-        }
-    }
+const menuRandomNumbers = confirm('Hello! Enter 5 Numbers to order and then i will show result on your viewport!')
+const randomNumbers1 = prompt('Enter 1st number');
+const randomNumbers2 = prompt('Enter 2st number');
+const randomNumbers3 = prompt('Enter 3st number');
+const randomNumbers4 = prompt('Enter 4st number');
+const randomNumbers5 = prompt('Enter 5st number');
+const mainRandomNumbers = [randomNumbers1 , randomNumbers2 , randomNumbers3 , randomNumbers4 , randomNumbers5];
+let mainResult2 = [];
+for (let i = 0; i < mainRandomNumbers.length; i++) {
+    mainResult2.push(mainRandomNumbers[i]**2);
 }
-if (bigObj.mainUser.progremmerSkills.html.allSkills === true) console.log(`${bigObj.mainUser.mainInformation.name} - agree`);
-else console.log(`${bigObj.mainUser.mainInformation.name} - disagree`);
-
-const firstObj = {
-    name: 'Artem' ,
-    age: 18
-}
-const secondObj = {...firstObj};
-secondObj.name = 'NOT Artem'
-console.log(secondObj);
+alert(mainResult2);
