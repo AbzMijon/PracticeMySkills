5e74d325a53b32bd2921a5a5e62679aacf5d5d4a
AbzMijon <egorchiktop228@gmail.com>
2022-04-24 18:36:57 +0300

    practice js

--- script.js
+++ script.js
@@ -120,3 +120,25 @@ alert(mainResult2); */
 // Practice Skills With JavaScript
 
 //1
+let userName = prompt('Create Name..');
+let userPass = prompt('Create Password..');
+const mainUser = {
+    name: userName ,
+    password: userPass ,
+}
+const confExit = confirm('Now You Exit From Account And You Repeat NAME and PASSWORD..)');
+let userNameCheck = prompt('Enter Name');
+let userPassCheck = prompt('Enter Password');
+const treatmentUserInfo = function () {
+    if (userNameCheck === mainUser.name && userPassCheck === mainUser.password) {
+        alert(`Welcome ${mainUser.name}`);
+        userStrForPalindrom = prompt('Okey , Now Enter Word To Checked To PALINDROM!');
+        let palindromCheck = userStrForPalindrom.split('').reverse().join('');
+        if (userStrForPalindrom === palindromCheck) alert(`${userStrForPalindrom} is a PALINDROM`);
+        else alert(`${userStrForPalindrom} is NOT a PALINDROM`);
+        mainUser.palindrom = userStrForPalindrom;
+    }
+    else alert('You Does Mistakes In NAME or PASSWORD');
+}
+treatmentUserInfo();
+console.log(mainUser);
