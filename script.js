8f2aee41d38c16502cf2ee4343bb2d63343f7d16
AbzMijon <egorchiktop228@gmail.com>
2022-04-27 22:15:42 +0300

    practice js skills

--- script.js
+++ script.js
@@ -12,52 +12,6 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
 });
 
-//Lesson TMS-FE-24onl
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Practice Skills With JavaScript
-
-//1
-/* let userName = prompt('Create Name..');
-let userPass = prompt('Create Password..');
-const mainUser = {
-    name: userName ,
-    password: userPass ,
-}
-const confExit = confirm('Now You Exit From Account And You Repeat NAME and PASSWORD..)');
-let userNameCheck = prompt('Enter Name');
-let userPassCheck = prompt('Enter Password');
-const treatmentUserInfo = function () {
-    if (userNameCheck === mainUser.name && userPassCheck === mainUser.password) {
-        alert(`Welcome ${mainUser.name}`);
-        userStrForPalindrom = prompt('Okey , Now Enter Word To Checked To PALINDROM!');
-        let palindromCheck = userStrForPalindrom.split('').reverse().join('');
-        if (userStrForPalindrom === palindromCheck) alert(`${userStrForPalindrom} is a PALINDROM`);
-        else alert(`${userStrForPalindrom} is NOT a PALINDROM`);
-        mainUser.palindrom = userStrForPalindrom;
-    }
-    else alert('You Does Mistakes In NAME or PASSWORD');
-}
-treatmentUserInfo();
-console.log(mainUser);
-
-//2
-function solution(str, ending){
-    if(str[str.length - 2] + str[str.length - 1] == ending) return true;
-    return false;
-}
-console.log(solution('abc' , 'c')); */
 
 const personExample = (name , workplace , country , send) => {
     this.name = name;
@@ -145,3 +99,15 @@ const testArr = [1, 2, 3, 4, 5, 111]
 for (const iterator of testArr) {
     console.log(iterator);
 }
+
+let defoltStr = 'abcdeou';
+let upgrateDefoltStr = defoltStr.replace(/[^abcd]/gi, '');
+console.log(upgrateDefoltStr);
+
+const firstFunc = (name , callback) => {
+    alert(`We need kill ${name}!`);
+    callback();
+}
+const callbackFunc = () => alert('Mission complete! Person was died!' + firstFunc.name);
+firstFunc('Влад' , callbackFunc)
+
