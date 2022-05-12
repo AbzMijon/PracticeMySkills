79424cf95e5c37452b01418c72660e06760bb8f9
AbzMijon <egorchiktop228@gmail.com>
2022-05-12 16:48:25 +0300

    meetings tasks

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ const swiper = new Swiper('.swiper', {
     },
     slidesPerView: 3,
 });
+
+//Meeting tasks
+
+//Palindrom
+let palindromStr = 'heh';
+(palindromStr === palindromStr.split('').reverse().join('')) ? console.log(true) : console.log(false);
+
+//FizzBuzz
+let someNum = 15;
+(someNum % 3 === 0 && someNum % 5 === 0) ? console.log('FizzBuzz') : (someNum % 3 === 0) ? console.log('Fizz') : (someNum % 5 === 0) ? console.log('Buzz') : console.log('Error!');
+
+//Vowels
+let vowelsStr = 'hello!';
+let vowelsStrResult = vowelsStr.replace(/[eyuioa]/gi, '');
+console.log(vowelsStr.length - vowelsStrResult.length);
